04a12772353e46d1e6797e248c07bc938d2a9685
Pholluxion <penalozadaniel00@gmail.com>
2023-12-04 17:01:11 -0500

    Initi WS

---
+++ bin/server.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, implicit_dynamic_list_literal
+
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+
+
+import '../routes/ws.dart' as ws;
+
+import '../routes/_middleware.dart' as middleware;
+
+void main() async {
+  final address = InternetAddress.anyIPv6;
+  final port = int.tryParse(Platform.environment['PORT'] ?? '8080') ?? 8080;
+  createServer(address, port);
+}
+
+Future<HttpServer> createServer(InternetAddress address, int port) async {
+  final handler = Cascade().add(buildRootHandler()).handler;
+  final server = await serve(handler, address, port);
+  print('\x1B[92m✓\x1B[0m Running on http://${server.address.host}:${server.port}');
+  return server;
+}
+
+Handler buildRootHandler() {
+  final pipeline = const Pipeline().addMiddleware(middleware.middleware);
+  final router = Router()
+    ..mount('/', (context) => buildHandler()(context));
+  return pipeline.addHandler(router);
+}
+
+Handler buildHandler() {
+  final pipeline = const Pipeline();
+  final router = Router()
+    ..all('/ws', (context) => ws.onRequest(context,));
+  return pipeline.addHandler(router);
+}
+
